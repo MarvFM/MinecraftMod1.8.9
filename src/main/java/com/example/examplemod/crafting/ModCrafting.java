6fcef75cd58b42fca9852bacaa82baa1e1f05713
Uneron <grosenick@emailn.de>
2016-07-09 12:24:19 +0200

    Crafting recipes to make everything

--- src/main/java/com/example/examplemod/crafting/ModCrafting.java
+++ src/main/java/com/example/examplemod/crafting/ModCrafting.java
@@ -3,6 +3,7 @@ package com.example.examplemod.crafting;
 import com.example.examplemod.blocks.ModBlocks;
 import com.example.examplemod.items.ModItems;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -27,18 +28,33 @@ public class ModCrafting {
                 "AA",
                 "AA",
                 'A', Blocks.dirt);
+
+        GameRegistry.addRecipe(new ItemStack(ModItems.testiumPickaxe, 1),
+                "TTT",
+                " S ",
+                " S ",
+                'T', ModBlocks.explodingBlock,
+                'S', Items.stick);
+
+        GameRegistry.addRecipe(new ItemStack(ModItems.testiumSword, 1),
+                "T",
+                "T",
+                "S",
+                'T', ModBlocks.explodingBlock,
+                'S', Items.stick);
     }
 
     private static void addShapelessRecipes(){
         //addShapelessRecipe(new ItemStack(output_Block/Item, amount(optional), metadata(optional)), input1, input2, ...)
         // inputs can either be items, blocks or stacks; stacks are always treated as single item
-        GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.explodingBlock, 2), Blocks.dirt,  Blocks.cobblestone);
+        GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.unrefinedTestium, 2), Blocks.dirt,  Blocks.cobblestone);
     }
 
     private static void addSmeltings(){
         //addSmelting(input_Block/Item, output_Block/Item, experienceamount)
         GameRegistry.addSmelting(Blocks.dirt, new ItemStack(ModItems.testItem), 1.0F);
         GameRegistry.addSmelting(Blocks.diamond_block, new ItemStack(ModBlocks.testBlock), 1.0F);
+        GameRegistry.addSmelting(ModBlocks.unrefinedTestium,  new ItemStack(ModBlocks.explodingBlock), 0.1F);
     }
 
 }
